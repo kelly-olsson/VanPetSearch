73738129b25f199a8cbe531a9a54119b48fa2196
Kelly Olsson <kolsson2002@gmail.com>
2021-08-18 16:06:23 -0700

    Added buttons to Search Page

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,19 @@
 import React from "react"
 
+// function handleClick() {
+//     fetch("")
+// }
+
 function Search() {
     return (
-        <div className='contact'>
+      <div className='contact'>
         <h1>Search</h1>
         <p>Please click on the search category to get started.</p>
+        <div>
+          <button onClick={handleClick}>Animals in Custody</button>
+          <button>Animals Found Deceased</button>
+          <button>Pets Reported Lost</button>
+        </div>
       </div>
     )
 }
