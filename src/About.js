a5b48b3d8b5e4988d19f9946dca6b77a325665ca
Kelly Olsson <kolsson2002@gmail.com>
2021-09-24 19:24:19 -0700

    Add 'About' section

--- src/About.js
+++ src/About.js
@@ -3,9 +3,16 @@ import React from "react"
 function About() {
     return (
         <div className='about'>
-        <h1>About Me</h1>
-        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+        <h1>Meet the Creator</h1>
+        <p>Kelly Olsson is currently a student at the British Columbia Institute of Technology. 
+          She is the owner of two precocious cats and knows the fear and anxiety that comes 
+          with having a pet escape. When she came across the well-maintained online data that 
+          Animal Control in the City of Vancouver keeps on animals, she came up
+          with the idea of VanPetSearch.</p>
+          <p>She hopes that the site will help worried pet owners in Vancouver to find answers
+            about their lost pets.</p>
+          <p>If you would like to make suggestions to improve the site, please don't hesitate to
+            contact her <a href="mailto:kolsson@gmail.com">here.</a></p>
       </div>
     )
 }
